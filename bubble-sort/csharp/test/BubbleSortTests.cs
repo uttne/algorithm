2c94f56c94965eac1d8981567f58b59f58eca5a3
uttne <uttne.mkr@gmail.com>
2022-03-30 00:03:31 +0900

    Array.Sort との比較テストを追加

--- bubble-sort/csharp/test/BubbleSortTests.cs
+++ bubble-sort/csharp/test/BubbleSortTests.cs
@@ -1,9 +1,20 @@
+using System;
+using System.Diagnostics;
+using System.Linq;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Algorithm.Tests;
 
 public class BubbleSortTests
 {
+    private readonly ITestOutputHelper _output;
+
+    public BubbleSortTests(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
     [Fact]
     public void Test()
     {
@@ -15,5 +26,38 @@ public class BubbleSortTests
         alg.Sort(list);
 
         Assert.Equal(expected, list);
+    }
+
+
+    [Fact]
+    public void SpeedTest()
+    {
+        var random = new Random(0);
+
+        var list = Enumerable.Range(0, 50000).Select(_ => random.Next()).ToArray();
+
+        var expected = list.ToArray();
+
+        var sw = new Stopwatch();
+
+        var alg = new BubbleSort();
+
+
+        sw.Restart();
+        Array.Sort(expected);
+        sw.Stop();
+        var runtimeSortTime = sw.Elapsed.TotalMilliseconds;
+
+        sw.Restart();
+        alg.Sort(list);
+        sw.Stop();
+        var mySortTime = sw.Elapsed.TotalMilliseconds;
+
+
+        Assert.Equal(expected, list);
+
+        _output.WriteLine($"Array.Sort               : {runtimeSortTime:0.000} [msec]");
+        _output.WriteLine($"Bubble Sort              : {mySortTime:0.000} [msec]");
+        _output.WriteLine($"Bubble Sort / Array.Sort : {(mySortTime * 100d / runtimeSortTime):0.000} [%]");
     }
 }
