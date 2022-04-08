9c6e85a5d59b2f862d1b4a464bbd051114be7559
uttne <uttne.mkr@gmail.com>
2022-04-08 23:08:08 +0900

    selection sort のプロジェクト準備

---
+++ selection-sort/csharp/src/SelectionSort.cs
@@ -0,0 +1,12 @@
+namespace Algorithm
+{
+    public class SelectionSort
+    {
+        public void Sort<T>(T[] list)
+        where T : IComparable<T>
+        {
+           
+           throw new NotImplementedException();
+        }
+    }
+}
