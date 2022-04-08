653bc401c9beab97b0afb38f316fb59367688b7b
uttne <uttne.mkr@gmail.com>
2022-04-08 23:22:06 +0900

    selection sort を実装

--- selection-sort/csharp/src/SelectionSort.cs
+++ selection-sort/csharp/src/SelectionSort.cs
@@ -5,8 +5,23 @@ namespace Algorithm
         public void Sort<T>(T[] list)
         where T : IComparable<T>
         {
-           
-           throw new NotImplementedException();
+            for (int i = 0; i < list.Length; ++i)
+            {
+                int minIndex = i;
+                for (int j = i; j < list.Length; ++j)
+                {
+                    var x = list[minIndex];
+                    var y = list[j];
+                    if (0 < x.CompareTo(y))
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                var swap = list[i];
+                list[i] = list[minIndex];
+                list[minIndex] = swap;
+            }
         }
     }
 }
