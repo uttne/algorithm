ecf327806dbf263b08fea137d8aa52e45e2f8004
uttne <uttne.mkr@gmail.com>
2022-04-13 23:00:59 +0900

    Insertion sort のプロジェクトを追加

---
+++ insertion-sort/csharp/src/InsertionSort.cs
@@ -0,0 +1,11 @@
+namespace Algorithm
+{
+    public class InsertionSort
+    {
+        public void Sort<T>(T[] list)
+        where T : IComparable<T>
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
