0d8ad34ea62d4e3e6152c96eda49aef9d28a7d72
uttne <uttne.mkr@gmail.com>
2022-04-13 23:31:28 +0900

    自分で考えた insertion sort の実装を追加

--- insertion-sort/csharp/src/InsertionSort.cs
+++ insertion-sort/csharp/src/InsertionSort.cs
@@ -5,7 +5,29 @@ namespace Algorithm
         public void Sort<T>(T[] list)
         where T : IComparable<T>
         {
-            throw new NotImplementedException();
+            for (int i = 1; i < list.Length; ++i)
+            {
+                var x = list[i];
+                var y = list[i - 1];
+
+                if (0 < x.CompareTo(y))
+                {
+                    continue;
+                }
+                list[i] = y;
+                int k = i - 2;
+                for (; 0 <= k; --k)
+                {
+                    y = list[k];
+                    if (x.CompareTo(y) < 0)
+                    {
+                        list[k + 1] = y;
+                        continue;
+                    }
+                    break;
+                }
+                list[k + 1] = x;
+            }
         }
     }
 }
